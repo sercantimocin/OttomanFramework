8fe3bc9b36ba788edfb030ce6a59ae9d9f72d99d
sercantimocin <sercantimocin@hotmail.com>
2016-06-21 00:01:54 +0300

    Authentication initial

---
+++ CrossCutting/Security/Ottoman.ItentityServer/Config/Clients.cs
@@ -0,0 +1,76 @@
+﻿namespace Ottoman.ItentityServer.Config
+{
+    using System.Collections.Generic;
+
+    using IdentityServer3.Core.Models;
+
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+             {
+                new Client
+                {
+                     ClientId = "tripgalleryclientcredentials",
+                     ClientName = "Trip Gallery (Client Credentials)",
+                     Flow = Flows.ClientCredentials,
+                     AllowAccessToAllScopes = true,
+
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret(Constants.TripGalleryClientSecret.Sha256())
+                    }
+                }
+                ,
+                new Client
+                {
+                     ClientId = "tripgalleryauthcode",
+                     ClientName = "Trip Gallery (Authorization Code)",
+                     Flow = Flows.AuthorizationCode,
+                     AllowAccessToAllScopes = true,
+
+                    // redirect = URI of the MVC application callback
+                    RedirectUris = new List<string>
+                    {
+                        Constants.TripGalleryMVCSTSCallback
+                    },           
+
+                    // client secret
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret(Constants.TripGalleryClientSecret.Sha256())
+                    }
+                }
+                ,
+                new Client
+                {
+                     ClientId = "tripgalleryimplicit",
+                     ClientName = "Trip Gallery (Implicit)",
+                     Flow = Flows.Implicit,
+                     AllowAccessToAllScopes = true,
+
+                    // redirect = URI of the Angular application callback page
+                    RedirectUris = new List<string>
+                    {
+                        Constants.TripGalleryAngular + "callback.html"
+                    }
+                }
+                ,
+                new Client
+                {
+                     ClientId = "tripgalleryropc",
+                     ClientName = "Trip Gallery (Resource Owner Password Credentials)",
+                     Flow = Flows.ResourceOwner,
+                     AllowAccessToAllScopes = true,
+
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret(Constants.TripGalleryClientSecret.Sha256())
+                    }
+                }
+
+             };
+        }
+    }
+}
---
+++ CrossCutting/Security/Ottoman.ItentityServer/Config/Users.cs
@@ -0,0 +1,30 @@
+﻿namespace Ottoman.ItentityServer.Config
+{
+    using System.Collections.Generic;
+
+    using IdentityServer3.Core.Services.InMemory;
+
+    public static class Users
+    {
+        public static List<InMemoryUser> Get()
+        {
+            return new List<InMemoryUser>() {
+                 
+                new InMemoryUser
+	            {
+	                Username = "Kevin",
+	                Password = "secret",                    
+	                Subject = "b05d3546-6ca8-4d32-b95c-77e94d705ddf"
+	             }
+	            ,
+	            new InMemoryUser
+	            {
+	                Username = "Sven",
+	                Password = "secret",
+	                Subject = "bb61e881-3a49-42a7-8b62-c13dbe102018"
+	            }  
+            };
+        }
+    }
+
+}
