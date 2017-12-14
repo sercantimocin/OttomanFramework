e2bbdd1174483c1a467dffb6b9c150c5c15cbc82
sercan.timocin <sercan.timocin@IT-102>
2017-12-14 17:13:38 +0300

    comments

--- Demo/Api/Demo.WebApi/Controllers/CustomerController.cs
+++ Demo/Api/Demo.WebApi/Controllers/CustomerController.cs
@@ -1,25 +1,27 @@
 ﻿namespace Demo.WebApi.Controllers
 {
-    using System.Collections.Generic;
-    using System.Web.Http;
-
     using Entities;
     using Models;
 
     using Ottoman.Core;
-    using Ottoman.Mapper.Extensions;
-    using Ottoman.MemoryCache;
 
     using Repository.Pattern.Repositories;
     using Repository.Pattern.UnitOfWork;
-
-    using Service.Pattern;
 
     /// <summary>
     /// The customer controller.
     /// </summary>
     public class CustomerController : OttomanController<Customer, CustomerDto>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerController"/> class.
+        /// </summary>
+        /// <param name="genericRepository">
+        /// The generic repository.
+        /// </param>
+        /// <param name="unitOfWork">
+        /// The unit of work.
+        /// </param>
         public CustomerController(IRepositoryAsync<Customer> genericRepository, IUnitOfWorkAsync unitOfWork)
             : base(genericRepository, unitOfWork)
         {
--- Demo/Api/Demo.WebApi/Controllers/OrderController.cs
+++ Demo/Api/Demo.WebApi/Controllers/OrderController.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace Demo.WebApi.Controllers
+﻿namespace Demo.WebApi.Controllers
 {
     using Demo.Entities;
     using Demo.WebApi.Models;
@@ -15,8 +8,20 @@ namespace Demo.WebApi.Controllers
     using Repository.Pattern.Repositories;
     using Repository.Pattern.UnitOfWork;
 
+    /// <summary>
+    /// The order controller.
+    /// </summary>
     public class OrderController : OttomanController<Order, OrderDto>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderController"/> class.
+        /// </summary>
+        /// <param name="genericRepository">
+        /// The generic repository.
+        /// </param>
+        /// <param name="unitOfWork">
+        /// The unit of work.
+        /// </param>
         public OrderController(IRepositoryAsync<Order> genericRepository, IUnitOfWorkAsync unitOfWork)
             : base(genericRepository, unitOfWork)
         {
